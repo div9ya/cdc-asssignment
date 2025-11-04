93471e25b98d75c33f73c1b4cbfeafa48406dd31
Divya Verma <divya2023verma@gmail.com>
2025-11-04 11:37:01 +0530

    Create Count Nodes in LL.java

---
+++ LinkedList introduction/Count Nodes in LL.java
@@ -0,0 +1,20 @@
+/*
+class Node{
+    int data;
+    Node next;
+    Node(int a){  data = a; next = null; }
+}*/
+
+class Solution {
+    public int getCount(Node head) {
+        // code here
+        int count=0;
+        Node temp=head;
+        while(temp!=null)
+        {
+            count+=1;
+            temp=temp.next;
+        }
+        return count;
+    }
+}
