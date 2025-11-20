b3f2fc5cf81f705db623bcdab33f4f1b5038321f
Divya Verma <divya2023verma@gmail.com>
2025-11-20 13:52:05 +0530

    Create ImplemetHash.java

---
+++ HashMap Introduction/ImplemetHash.java
@@ -0,0 +1,50 @@
+class MyHashMap {
+    List<Integer> keys;
+    List<Integer> values;
+    public MyHashMap() {
+        keys=new ArrayList<>();
+        values=new ArrayList<>();
+    }
+    
+    public void put(int key, int value) {
+        int index=keys.indexOf(key);
+        if(index!=-1)
+        {
+            values.set(index,value);
+        }
+        else
+        {
+            keys.add(key);
+            values.add(value);
+        }
+    }
+    
+    public int get(int key) {
+        int index=keys.indexOf(key);
+        if(index==-1)
+        {
+            return -1;
+        }
+        else
+        {
+            return values.get(index);
+        }
+    }
+    
+    public void remove(int key) {
+        int index=keys.indexOf(key);
+        if(index!=-1)
+        {
+            keys.remove(index);
+            values.remove(index);
+        }
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
